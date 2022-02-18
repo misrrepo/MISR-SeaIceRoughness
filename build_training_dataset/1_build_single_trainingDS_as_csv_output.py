b421e3e6793dc20cf8bcfbbe0489b2bd983210fc
ehsaanmosadegh <emosadegh@nevada.unr.edu>
2022-02-18 13:11:35 -0800

    removed dt from build code

--- build_training_dataset/1_build_single_trainingDS_as_csv_output.py
+++ build_training_dataset/1_build_single_trainingDS_as_csv_output.py
@@ -43,25 +43,22 @@ else:
 
 ##- set up paths
 #################### input dir should include 9 dir
-# masked_toa_home = "/media/ehsan/Gdrive_18TB/all_masked_toa_refl_2010_2014_2019"
-masked_toa_home = "/media/ehsan/6T_part1/2016/april_2016/14528_apr2016/project_april_2016_9cam3bands/masked_toa_refl_april_2016_9cam4bands_day1_30_p1_233_b1_46"
+masked_toa_home = "/media/ehsan/Gdrive_18TB/all_masked_toa_refl_2010_2014_2019"
+# masked_toa_home = "/media/ehsan/6T_part1/2016/april_2016/14528_apr2016/project_april_2016_9cam3bands/masked_toa_refl_april_2016_9cam4bands_day1_30_p1_233_b1_46"
 
 
-# atm_dir = "/home/ehsan/misr_lab/ATM_2010_2014_2019"
-atm_dir = "/media/ehsan/6T_part1/2016/april_2016/14528_apr2016/project_april_2016_3cam/ATM_apr2016_5days"
+atm_dir = "/home/ehsan/misr_lab/ATM_2010_2014_2019"
+# atm_dir = "/media/ehsan/6T_part1/2016/april_2016/14528_apr2016/project_april_2016_3cam/ATM_apr2016_5days"
 
 
 #################### output
-# trainingDS_dir = "/media/ehsan/Gdrive_18TB/training_DS_2010_2014_2019"
-trainingDS_dir = "/media/ehsan/6T_part1/2016/april_2016/14528_apr2016/project_april_2016_3cam/training_data/april_2016"
-
-
-# output_final_ds_label = "april_2016_9cam3bands_final_merged_dataset.csv"
+trainingDS_dir = "/media/ehsan/Gdrive_18TB/training_DS_2010_2014_2019"
+# trainingDS_dir = "/media/ehsan/6T_part1/2016/april_2016/14528_apr2016/project_april_2016_3cam/training_data/april_2016"
 
 
 #################### setup pattern- do not change
 atm_file_pattern = 'ILATM2*'+'.csv'
-single_csv_ds_label = "out9cam3bands"
+single_csv_ds_label = "9cam3bands"
 
 
 # atm_file_sample = 'ILATM2_20160420_175457_smooth_nadir3seg_50pt.csv'
@@ -82,8 +79,10 @@ t1 = dt.datetime.now()
 
 ds_row_index = 0
 
-column_names = ['path','orbit','block','line','sample','lat','lon','Da_r','Ca_r','Ba_r','Aa_r',\
-				'An_r','An_g','An_b','An_nir','Af_r','Bf_r','Cf_r','Df_r','mean_ATM_roughness','ATM_start_time','ATM_end_time']
+column_names = ['path','orbit','block','line','sample','lat','lon',\
+				'Da_r','Ca_r','Ba_r','Aa_r','An_r','An_g','An_b','An_nir','Af_r','Bf_r','Cf_r','Df_r',\
+				'mean_ATM_roughness']#,'ATM_start_time','ATM_end_time']
+
 
 final_ds_list = []
 
@@ -100,7 +99,7 @@ print('total ATM.csv files: %d' %total_atm_files_found)
 # final_ds.set_index(total_atm_files_found*10000)
 
 
-# open each ATM file
+# open each ATM file and read each row and will eventually write out a train dataset for each ATM file
 for atm_cntr, ATMfile in enumerate(atm_list):
 
 	print('\n********** processing ATM file: (%d/%d)' %(atm_cntr+1, len(atm_list)))
@@ -197,148 +196,141 @@ for atm_cntr, ATMfile in enumerate(atm_list):
 			if misr_pathNumber_today not in paths_cover_ATM_location:
 				# print('MISR path %d in day %s not match with path that ATM lat-lon falls in, continue to next ATM latLon row!' %(misr_pathNumber_today, atm_day))
 				continue    # to next location == latLon
-			# else:
-				print('MISR path %d covers ATM location- we will find Block-Line-Sample!' %misr_pathNumber_today)
-
+			
+			else:
+				# print('MISR path %d covers ATM location- we will find Block-Line-Sample!' %misr_pathNumber_today)
 	
-			# find MISR pixel from that
-			resolution_meters = 275
-			
-			misr_block, misr_pixel_x, misr_pixel_y = mtk.latlon_to_bls(misr_pathNumber_today, resolution_meters, atm_lat, atm_lon) # q- why crash? these paths cover that that ATM location
-			
-			# print('block: %s' %misr_block)
-			# print('pixel x: %s' %misr_pixel_x)
-			# print('pixel y: %s' %misr_pixel_y)
-
-
-			path_num_str = str(misr_pathNumber_today).zfill(3)
-			misr_orbit_str = str(misr_orbit_today).zfill(6) 
-			misr_block_str = str(misr_block).zfill(3)
-
-
-			# print('path-str: %s' %path_num_str)
-			# print('orbit-str: %s' %misr_orbit_str)
-			# print('block-str: %s' %misr_block_str)
-
-
-			#### create fulll path to 9 cameras and 4 spectral bands
-
-			# find MISR files associated with P-O-B: masked_toa_P_O_B
-			# aft/back cameras
-			aa_red_fp = os.path.join(masked_toa_home,'Aa', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_aa_red.dat'))
-			ba_red_fp = os.path.join(masked_toa_home,'Ba', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_ba_red.dat'))
-			ca_red_fp = os.path.join(masked_toa_home,'Ca', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_ca_red.dat'))
-			da_red_fp = os.path.join(masked_toa_home,'Da', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_da_red.dat'))
-		   
-			# front cameras
-			af_red_fp = os.path.join(masked_toa_home,'Af', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_af_red.dat'))
-			bf_red_fp = os.path.join(masked_toa_home,'Bf', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_bf_red.dat'))
-			cf_red_fp = os.path.join(masked_toa_home,'Cf', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_cf_red.dat'))
-			df_red_fp = os.path.join(masked_toa_home,'Df', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_df_red.dat'))
-			
-			# nadir
-			an_red_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_red.dat'))
-			an_green_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_green.dat'))
-			an_blue_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_blue.dat'))
-			an_nir_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_NIR.dat')) # note here: should match the file names on drive ("an_NIR" is ok, not "an_nir")
-
-			#### check if camera files availabel
-
-
-			# create a list of camreas based on order from Da->Df- order if important 
-			masked_toaFile_orderedList = [da_red_fp, ca_red_fp, ba_red_fp, aa_red_fp,
-										  an_red_fp, an_green_fp, an_blue_fp, an_nir_fp, 
-										  af_red_fp, bf_red_fp, cf_red_fp, df_red_fp]
-
-
-
-			available_file_status = []
-			# check file exists
-			for masked_toa_file in masked_toaFile_orderedList:
-				# print(masked_toa_file)
-				available_status = os.path.isfile(masked_toa_file)
-				# print(available_status)
-				available_file_status.append(available_status)
-
-
-			##-- check if all 12 cameras are available
-			# if (all(available_file_status)==False): # check if any element is false==file not found
-			#     # print('***some masked-files missing- continue*** \n')
-			#     continue # to the next ATM row
-
-			if (not any(available_file_status)):
-				# print('did not find even a single camera value for ATM location/row- continue')
-				continue
-
-			# print("num. of masked toa files found:")
-			# print(available_file_status)
-
-
-
-			# open masked-toa-refl and extract pixel value
-			pixel_values = []
-
-			for maskedTOA_rawfile in masked_toaFile_orderedList:
-				# print(maskedTOA_rawfile)
-				is_file = os.path.isfile(maskedTOA_rawfile)
-				# print(is_file)
+				# find MISR pixel from that
+				resolution_meters = 275
 				
-				#-- if a camera is missing, assign fill value for cameras that are not found
-				if (is_file==False):
-					pixel_values.append(-999.0) 
-				else:
-					rough_2d_arr = np.fromfile(maskedTOA_rawfile, dtype=np.double)[0:1048576].reshape((512,2048))   # 'double==float64'     # is this roughness in cm?
-					# print(rough_2d_arr.shape)
-					# print(type(rough_2d_arr))
-					##- maybe make a image from it?
-					pixel_value = rough_2d_arr[int(misr_pixel_x), int(misr_pixel_y)]
-					pixel_values.append(round(pixel_value,5))
+				misr_block, misr_pixel_x, misr_pixel_y = mtk.latlon_to_bls(misr_pathNumber_today, resolution_meters, atm_lat, atm_lon) # q- why crash? these paths cover that that ATM location
+				
+				# print('block: %s' %misr_block)
+				# print('pixel x: %s' %misr_pixel_x)
+				# print('pixel y: %s' %misr_pixel_y)
 
 
-			# print('path-str: %s' %path_num_str)
-			# print('orbit-str: %s' %misr_orbit_str)
-			# print('block-str: %s' %misr_block_str)
+				path_num_str = str(misr_pathNumber_today).zfill(3)
+				misr_orbit_str = str(misr_orbit_today).zfill(6) 
+				misr_block_str = str(misr_block).zfill(3)
 
-			# print("found these pixel values for an ATM location:")
-			# print(pixel_values)
 
-			########################################################
-			# new way of making a list and then open it in dataframe
+				# print('path-str: %s' %path_num_str)
+				# print('orbit-str: %s' %misr_orbit_str)
+				# print('block-str: %s' %misr_block_str)
 
-			final_ds_values = [misr_pathNumber_today, misr_orbit_today, misr_block,\
-			 int(misr_pixel_x), int(misr_pixel_y), round(atm_lat,7), round(atm_lon,7), \
-			 pixel_values[0],pixel_values[1],pixel_values[2],pixel_values[3],pixel_values[4],\
-			 pixel_values[5],pixel_values[6],pixel_values[7],pixel_values[8],pixel_values[9],\
-			 pixel_values[10],pixel_values[11],atm_roughness,\
-			 ATM_start_time_hrminsec,ATM_end_time_hrminsec]
-			
 
-			zipped = zip(column_names, final_ds_values)
-			a_dictionary = dict(zipped)
-			# print(a_dictionary)
-			final_ds_list.append(a_dictionary)
-			
-			ds_row_index +=1
+				#### create fulll path to 9 cameras and 4 spectral bands
 
-	# ## write each csf file in he loop
-	# print("1st row of data:")
-	# print(final_ds_list[0])
+				# find MISR files associated with P-O-B: masked_toa_P_O_B
+				# aft/back cameras
+				aa_red_fp = os.path.join(masked_toa_home,'Aa', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_aa_red.dat'))
+				ba_red_fp = os.path.join(masked_toa_home,'Ba', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_ba_red.dat'))
+				ca_red_fp = os.path.join(masked_toa_home,'Ca', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_ca_red.dat'))
+				da_red_fp = os.path.join(masked_toa_home,'Da', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_da_red.dat'))
+			   
+				# front cameras
+				af_red_fp = os.path.join(masked_toa_home,'Af', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_af_red.dat'))
+				bf_red_fp = os.path.join(masked_toa_home,'Bf', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_bf_red.dat'))
+				cf_red_fp = os.path.join(masked_toa_home,'Cf', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_cf_red.dat'))
+				df_red_fp = os.path.join(masked_toa_home,'Df', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_df_red.dat'))
+				
+				# nadir
+				an_red_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_red.dat'))
+				an_green_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_green.dat'))
+				an_blue_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_blue.dat'))
+				an_nir_fp = os.path.join(masked_toa_home,'An', ('masked_toa_refl_P'+path_num_str+'_'+'O'+misr_orbit_str+'_'+'B'+misr_block_str+'_an_NIR.dat')) # note here: should match the file names on drive ("an_NIR" is ok, not "an_nir")
+
+				#### check if camera files availabel
+
+
+				# create a list of camreas based on order from Da->Df- order if important 
+				masked_toaFile_orderedList = [da_red_fp, ca_red_fp, ba_red_fp, aa_red_fp,
+											  an_red_fp, an_green_fp, an_blue_fp, an_nir_fp, 
+											  af_red_fp, bf_red_fp, cf_red_fp, df_red_fp]
+
+
+
+				available_file_status = []
+				# check file exists
+				for masked_toa_file in masked_toaFile_orderedList:
+					# print(masked_toa_file)
+					available_status = os.path.isfile(masked_toa_file)
+					# print(available_status)
+					available_file_status.append(available_status)
+
+
+				##-- check if all 12 files/cameras are available
+
+				# if (all(available_file_status)==False): # check if any element is false==file not found
+				#     # print('***some masked-files missing- continue*** \n')
+				#     continue # to the next ATM row
+
+				if (not any(available_file_status)):
+					# print('did not find even a single camera value for ATM location/row- continue')
+					continue
+
+				# print("num. of masked toa files found:")
+				# print(available_file_status)
+
+
+
+				# open masked-toa-refl and extract pixel value
+				pixel_values = []
+
+				for maskedTOA_rawfile in masked_toaFile_orderedList:
+					# print(maskedTOA_rawfile)
+					is_file = os.path.isfile(maskedTOA_rawfile)
+					# print(is_file)
+					
+					#-- if one camera is missing, assign fill value for that camera that was not found
+					if (is_file==False):
+						pixel_values.append(-999.0) 
+					else:
+						rough_2d_arr = np.fromfile(maskedTOA_rawfile, dtype=np.double)[0:1048576].reshape((512,2048))   # 'double==float64'     # is this roughness in cm?
+						# print(rough_2d_arr.shape)
+						# print(type(rough_2d_arr))
+						##- maybe make a image from it?
+						pixel_value = rough_2d_arr[int(misr_pixel_x), int(misr_pixel_y)]
+						pixel_values.append(round(pixel_value,5))
+
+
+				# print('path-str: %s' %path_num_str)
+				# print('orbit-str: %s' %misr_orbit_str)
+				# print('block-str: %s' %misr_block_str)
+
+				# print("found these pixel values for an ATM location:")
+				# print(pixel_values)
+
+				########################################################
+				# new way of making a list and then open it in dataframe
+
+				final_ds_values = [misr_pathNumber_today, misr_orbit_today, misr_block,\
+				 int(misr_pixel_x), int(misr_pixel_y), round(atm_lat,7), round(atm_lon,7), \
+				 pixel_values[0],pixel_values[1],pixel_values[2],pixel_values[3],pixel_values[4],\
+				 pixel_values[5],pixel_values[6],pixel_values[7],pixel_values[8],pixel_values[9],\
+				 pixel_values[10],pixel_values[11],atm_roughness]#,\
+				 # ATM_start_time_hrminsec,ATM_end_time_hrminsec]
+				
+
+				zipped = zip(column_names, final_ds_values)
+				a_dictionary = dict(zipped)
+				# print(a_dictionary)
+				final_ds_list.append(a_dictionary)
+				
+				ds_row_index +=1
+
 
 	##################################################################
-
-	# print("processed %d rows" %ds_row_index)
-	# if (ds_row_index == 0):
-
-	##################################################################
-
+	# write out the train dataset for each ATM file 
 
 	if (len(final_ds_list) == 0):
 		print(final_ds_list)
 		print("ATM list is empty, we write -998 flag & continue to next ATM file")
-		print("note: zero means we did not find any MISR pixel for locations in an ATM file.")
+		print("note: -998 means we did not find any MISR pixel that covers ATM locations in that file.")
 
 		
-		final_ds_list = [-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998]
+		final_ds_list = [-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998,-998]#,-998,-998]
 		
 		with open(output_file_fp, 'w', encoding='UTF8', newline='') as fileObj:
 			writer = csv.writer(fileObj)  # create a new instance of the DictWriter class by passing the file object
@@ -346,27 +338,10 @@ for atm_cntr, ATMfile in enumerate(atm_list):
 			writer.writerow(final_ds_list)
 
 		final_ds_list.clear()
-		continue
+		# continue
+		print(output_file_fp)
 
 	else:
-		## dataframe method
-		# final_df = pd.DataFrame(columns=column_names)
-		# final_df = final_df.append(final_ds_list, ignore_index=True) # add list to existing dataframe; True: preserve the DataFrame indices; 
-
-		# out_DS  = os.path.join(trainingDS_dir, output_ds_label)
-		# final_df.to_csv(out_DS)
-		# print(out_DS)
-		# final_df=None
-		# final_df.drop(columns=column_names, axis=1, inplace=True)
-
-		## csv writer method
-		# ## if each row is a list 
-		# with open (output_file_fp, "w") as fileObj:
-		# 	writer = csv.writer(fileObj)
-		# 	writer.writerow(column_names)
-		# 	writer.writerows(final_ds_list)
-
-		## if ech row is a dictionary
 		print('writing out single CSV file at a time...')
 		
 		with open(output_file_fp, 'w', encoding='UTF8', newline='') as fileObj:
@@ -407,3 +382,23 @@ print("\nFINISHED SUCCESSFULLY: we processed %d csv files. In next step we shoul
 
 #########################################################################################################################################################
 
+######## old code
+		## dataframe method
+		# final_df = pd.DataFrame(columns=column_names)
+		# final_df = final_df.append(final_ds_list, ignore_index=True) # add list to existing dataframe; True: preserve the DataFrame indices; 
+
+		# out_DS  = os.path.join(trainingDS_dir, output_ds_label)
+		# final_df.to_csv(out_DS)
+		# print(out_DS)
+		# final_df=None
+		# final_df.drop(columns=column_names, axis=1, inplace=True)
+
+		## csv writer method
+		# ## if each row is a list 
+		# with open (output_file_fp, "w") as fileObj:
+		# 	writer = csv.writer(fileObj)
+		# 	writer.writerow(column_names)
+		# 	writer.writerows(final_ds_list)
+
+
+		## if each row is a dictionary
