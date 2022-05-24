fb214c04b474ed0f39ede30065e2eab2c9c2b09f
Ehsan Mosadegh <ehsanmos@Ehsans-MacBook-Pro.local>
2022-05-23 18:46:15 -0700

    toa-april2016

--- toa_rad_to_refl_to_blocks/run_TOARad2Refl4AllBlocks_allCameras_april2016.py
+++ toa_rad_to_refl_to_blocks/run_TOARad2Refl4AllBlocks_allCameras_april2016.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/env python
 '''
 author: Ehsna Mosadegh (emosadegh@nevada.unr.edu)
 
@@ -34,18 +34,23 @@ import MisrToolkit as MTK
 
 #===== input directory #=====
 #-- input_storage_path: is where we stored hdf data for each project in sub-directories under this directories. subdirectories can be data for each month. hdf radiance files reflectance (GRP_ELLIPSOID) files, where we downloaded files
-#-- where MISR ellipsoid files are
-input_dir_fullpath = "/media/ehsan/6T_part1/14528_apr2016/misr_hdf_files/misr_ellipsoid_files"
-#-- where toa-refl.dat files go
-output_path = "/media/ehsan/6T_part1/14528_apr2016/project_april_2016_9cam3bands"
+#-- where MISR ellipsoid/hdf files are
+input_dir_fullpath = "/data/gpfs/assoc/misr_roughness/2016/april_2016/hdf_files/hdf_inrange_15_30_april2016_9cams"
 
-exe_dir = "/home/ehsan/misr_lab/MISR-SeaIceRoughness/exe_dir"
+
+#-- where toa-refl.dat files go
+output_path = "/data/gpfs/assoc/misr_roughness/2016/april_2016/toa_refl_april2016_9cams_day15_30_p1to233_b1to46"
+
+
+exe_dir = "/data/gpfs/home/emosadegh/MISR-SeaIceRoughness/exe_dir"
 exe_name = 'TOARad2Refl4AllBlocks_allCameras'
+
 
 year = 2016
 month = 4
 day_range = [1,30]		# this code checks day-range and skips files w/days that are not in this range
 block_range = [1,46] 	# [start, stop]; should match with block range in downloading step
+
 
 #~ output file labling- rename them based on your project
 month_label = "april_2016_9cam4bands"
