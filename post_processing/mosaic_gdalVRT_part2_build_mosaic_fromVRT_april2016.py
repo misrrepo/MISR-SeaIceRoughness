8e13c4e3fcf209df7983f44d2e2f687a8800562c
misr_repo <MISR.repo@gmail.com>
2022-08-04 12:57:05 -0700

    set path for mosaic build

--- post_processing/mosaic_gdalVRT_part2_build_mosaic_fromVRT_april2016.py
+++ post_processing/mosaic_gdalVRT_part2_build_mosaic_fromVRT_april2016.py
@@ -10,20 +10,17 @@ import datetime as dt
 t1 = dt.datetime.now()
 
 # vrt directory
-raster_dir = '/Users/ehsanmos/MLP_dataset/21_april_2016_all_polar_rasters_inOrder/blocks_20_to_46'
+raster_dir = '/data/gpfs/assoc/misr_roughness/2016/april_2016/predict_roughness_k_zero_npts_10/all_polar_rasters/blocks_20_46/polar_raster_subdir_2016_4_15'
 
 
+input_vrt_filename = 'vrt_average_mosaic_gdal_15_april_2016_blocks_20_46.vrt'
 
-input_vrt_filename = 'vrt_average_mosaic_gdal_blocks_20_to_46.vrt'
-
-output_mosaic_filename = 'mosaic_gdal_blocks_20_to_46_int16.tif'
-
-
-output_mosaic_fp = os.path.join(raster_dir, output_mosaic_filename)
-
+output_mosaic_filename = 'mosaic_gdal_15_april_2016_blocks_20_46.tif'
 
 #*****************************************************
 ## remembr to update & rename VRT file 
+
+output_mosaic_fp = os.path.join(raster_dir, output_mosaic_filename)
 input_VRT_fp = os.path.join(raster_dir, input_vrt_filename)
 
 if (os.path.isfile(input_VRT_fp)!=True):
@@ -39,7 +36,7 @@ gdal.SetConfigOption('GDAL_VRT_ENABLE_PYTHON', 'YES') # to be able to include py
 mosaic_ds = gdal.Translate(output_mosaic_fp,
                             input_VRT_fp,
                             format = 'GTiff',
-                            noData = -99.0, # also consider -99991.0 which is 2 black sides of MISR block
+                            noData = -99.0 -99991.0,    # also consider -99991.0 which is 2 black sides of MISR block
                             resampleAlg = resamplingAlg,
                             outputType = gdal.GDT_Int16) # note: input dtype is float_64==double, maybe here change dtype to make it smaller img???? # outputType = gdal.GDT_Byte     
 
