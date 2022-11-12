5b74608ac1b67028670ffd33ddf9178f48fe26c5
misr_repo <MISR.repo@gmail.com>
2022-11-11 16:57:38 -0800

    mod cmd

--- cloud_mask/run_ReadMISRCloudMask.py
+++ cloud_mask/run_ReadMISRCloudMask.py
@@ -87,7 +87,7 @@ for file_count, file in enumerate(files_list):
 		ofile = out_dir_fullpath + '/' + 'cloudmask_' + path + '_' + orbit + '_B%03d.msk' % block
 		print(type(ofile))
 
-		cmd = ("%s" "%s" "%s" "%s" "%s" %(exe_dir_fullpath, ifile, block, ofile, cloudmask_filetype))
+		cmd = (' "%s" "%s" "%s" "%s" "%s" ' %(exe_dir_fullpath, ifile, block, ofile, cloudmask_filetype))
 		
 		print("checkpoint-1")
 		sys.stderr.write('%5d: %s\n' % (n + 1, cmd)) # n+1 to count input files on screen
