b95d49ae47dbb5ccd0a477ca9602f0b8bb719e4d
misr_repo <MISR.repo@gmail.com>
2022-11-11 22:26:17 -0800

    cmd

--- cloud_mask/ReadMISRCloudMask.c
+++ cloud_mask/ReadMISRCloudMask.c
@@ -533,12 +533,12 @@ int main(int argc, char *argv[])
 
 	printf("issue here-6\n");
 
-	// strcpy(cloudmaskname, argv[4]); // copy argv[4] to cloudmaskname
-	cloudmaskname = argv[4]; // copy argv[4] to cloudmaskname
+	strcpy(cloudmaskname, argv[4]); // copy argv[4] to cloudmaskname
+	// cloudmaskname = argv[4]; // copy argv[4] to cloudmaskname
 
 	printf("issue here-7\n");
 
-	if (!readMISRCloudMask(fname[0], cloudmaskname)) return 1;
+	if (!readMISRCloudMask(fname[0], *cloudmaskname)) return 1;
 
 	if (!write_data(fname[1], cmask0_ptr, 512, 2048)) return 1; // E- we only write cmask0_ptr data as output! all elements are checked to be total of (512*2048)
 
