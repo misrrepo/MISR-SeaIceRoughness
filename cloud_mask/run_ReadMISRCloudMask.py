f42cb91c88b34ddb38dd239d2fbf6380c9bb03b9
misr_repo <MISR.repo@gmail.com>
2022-11-12 16:38:32 -0800

    cmd

--- cloud_mask/run_ReadMISRCloudMask.py
+++ cloud_mask/run_ReadMISRCloudMask.py
@@ -92,7 +92,7 @@ for file_count, file in enumerate(files_list):
 
 
 		# cmd = ('%s %s %s %s %s') %(exe_dir_fullpath, ifile, block, ofile, cloudmask_filetype)  # a sequence of arguments is generally preferred,
-		cmd = [exe_dir_fullpath, ifile, block, ofile, cloudmask_filetype]
+		cmd = [exe_dir_fullpath, ifile, str(block), ofile, cloudmask_filetype]
 		print(cmd)
 		print(type(cmd))
 		print(len(cmd))
