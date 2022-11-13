8fc2a6ef5e29ab1cfaad8b8e5676d73f3ca76e0e
misr_repo <MISR.repo@gmail.com>
2022-11-12 16:36:03 -0800

    cmd

--- cloud_mask/run_ReadMISRCloudMask.py
+++ cloud_mask/run_ReadMISRCloudMask.py
@@ -103,7 +103,7 @@ for file_count, file in enumerate(files_list):
 
 		# run the cmd command
 		print("run cmd...")
-		return_value_of_cmd = subprocess.call(cmd)  # If passing a single string, either shell must be True (shell=True)
+		return_value_of_cmd = subprocess.call(cmd, shell=True)  # If passing a single string, either shell must be True (shell=True)
 		print("out of C ...")
 		print(return_value_of_cmd)
 		
