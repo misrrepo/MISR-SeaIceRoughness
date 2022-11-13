bf0fb2f75c00bbdb0ea284edbd77d3050c094cd0
misr_repo <MISR.repo@gmail.com>
2022-11-12 16:37:01 -0800

    cmd

--- cloud_mask/run_ReadMISRCloudMask.py
+++ cloud_mask/run_ReadMISRCloudMask.py
@@ -96,6 +96,8 @@ for file_count, file in enumerate(files_list):
 		print(cmd)
 		print(type(cmd))
 		print(len(cmd))
+		for icmd in cmd:
+			print(type(icmd))
 
 		# print("checkpoint-1")
 		sys.stderr.write('%5d: %s\n' % (n + 1, cmd)) # n+1 to count input files on screen
